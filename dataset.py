ab133d8cf1dba1a904408b946f6214de9abe1ff8
Bilal-Trigui <bilaltrigui06@gmail.com>
2026-02-01 17:12:12 -0500

    Updated Dataset Class

--- dataset.py
+++ dataset.py
@@ -1,22 +1,23 @@
 #import libraries
-import pathlib as pl
 import numpy as np
 import torch
 import librosa
 from torch.utils.data import Dataset
 
-class dataset(Dataset):
-    def __init__(self, root, segment_length=0, sr=44100):
+class dataSet(Dataset):
+    def __init__(self, root, segment_length=6, sr=44100, n_fft=1024, hop_length=256):
         self.root = Path(root)
         self.tracks = list(self.root.glob("*"))
         self.sr = sr
         self.segment_length = segment_length
+        self.n_fft = n_fft
+        self.hop_length = hop_length
 
     def __len__(self):
         return len(self.tracks)
 
     def __getitem__(self, idx):
-        track = self.tracks[idx % lem(self.tracks)]
+        track = self.tracks[idx % len(self.tracks)]
 
         mix, _ = librosa.load(track / "mixture.wav", sr=self.sr, mono=True)
         vocals, _ = librosa.load(track / "vocals.wav", sr=self.sr, mono=True)
@@ -24,8 +25,8 @@ class dataset(Dataset):
         bass, _ = librosa.load(track / "bass.wav", sr=self.sr, mono=True)
         other, _ = librosa.load(track / "other.wav", sr=self.sr, mono=True)
 
-        length = self.serment_length * self.sr
-        start = np.random.randint(0, len(mix) = length)
+        length = self.segment_length * self.sr
+        start = np.random.randint(0, len(mix) - length)
 
         mix = mix[start:start+length]
         vocals = vocals[start:start+length]
@@ -33,9 +34,9 @@ class dataset(Dataset):
         bass = bass[start:start+length]
         other = other[start:start+length]
 
-        return torch.tensor(mix), torch.stack([
-            torch.tensor(vocals)
-            torch.tensor(drums)
-            torch.tensor(bass)
-            torch.tensor(other)
+        return torch.tensor(np.abs(librosa.stft(mix, n_fft=self.n_fft, hop_length=self.hop_length))), torch.stack([
+            torch.tensor(np.abs(librosa.stft(vocals, n_fft=self.n_fft, hop_length=self.hop_length)))
+            torch.tensor(np.abs(librosa.stft(drums, n_fft=self.n_fft, hop_length=self.hop_length)))
+            torch.tensor(np.abs(librosa.stft(bass, n_fft=self.n_fft, hop_length=self.hop_length)))
+            torch.tensor(np.abs(librosa.stft(other, n_fft=self.n_fft, hop_length=self.hop_length)))
         ])
--- sepModel.py
+++ sepModel.py
@@ -5,10 +5,12 @@ import torchaudio as ta
 import numpy as np
 import torch
 import librosa
+from dataset import dataSet as ds
 
 #Creates array of audio files that will be used for model training
-root = pl.Path("/media/bilal/HardDrive2/musdb18hq/train/")
-tracks = list(root.glob("*"))
+train_root = pl.Path("/media/bilal/HardDrive2/musdb18hq/train/")
+val_root = pl.Path("/media/bilal/HardDrive2/musdb18hq/validation/")
+test_root = pl.Path("/media/bilal/HardDrive2/musdb18hq/test/")
 
-
+trackset = Dataset(train_root)
 
